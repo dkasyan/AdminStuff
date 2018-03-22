e9768ae6dd35ba592b2049c90792bee981ea1dfa
Damian Kasyan <damian.kasyan@hotmail.com>
2018-03-22 16:43:32 +0100

    Update storestorm_centOS_7.sh
    Dodane kasowanie i long

--- storestorm_centOS_7.sh
+++ storestorm_centOS_7.sh
@@ -12,8 +12,27 @@ yum net-tools -y
 yum smartmontools -y
 lsblk 
 lsblk > str.txt
-smartctl -t short /dev/sda > iaa.txt
-smartctl -t short /dev/sdb > ibb.txt
-smartctl -t shrot /dev/sdc > icc.txt
-smartctl -t short /dev/sdd > idd.txt
+sudo smartctl -t short /dev/sda > iaa.txt
+sudo smartctl -t short /dev/sdb > ibb.txt
+sudo smartctl -t shrot /dev/sdc > icc.txt
+sudo smartctl -t short /dev/sdd > idd.txt
+sleep 5m
+sudo smartctl -t long /dev/sda > iaal.txt
+sudo smartctl -t long /dev/sdb > ibbl.txt
+sudo smartctl -t long /dev/sdc > iccl.txt
+sudo smartctl -t long /dev/sdd > iddl.txt
+sleep 400m
+sudo smartctl -a /dev/sda > testa.txt
+sudo smartctl -a /dev/sdb > testb.txt
+sudo smartctl -a /dev/sdc > testc.txt
+sudo smartctl -a /dev/sdd > testd.txt
+#Kasowanie danych na dyskach
+sudo shred -vf -n1 /dev/sda >dela.txt
+sudo shred -vf -n1 /dev/sdb >delb.txt
+sudo shred -vf -n1 /dev/sdc >delc.txt
+sudo shred -vf -n1 /dev/sdd >deld.txt
+echo"Skrypt został wykonany poprawnie"
+
+
+
 
