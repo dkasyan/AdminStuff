2c12499cdbc8685464c6b8de7575bbbdffd54e3e
Damian Kasyan <damian.kasyan@hotmail.com>
2018-03-23 10:07:54 +0100

    Dla Debiana

---
+++ storestorm_debian_9.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Komentarze dodaje się przez hasztag
+
+echo "Bede instalował"
+echo "Czy tego chcesz czy nie"
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install vim -y
+sudo apt-get install mc -y
+sudo apt-get install net-tools -y
+sudo apt-get install smartmontools -y 
+#Instalacja FTP
+apt-get install proftpd nanp -y
+service proftpd restart
+echo "Skończyłem"
