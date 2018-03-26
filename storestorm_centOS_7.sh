7ad9eaee9189b6882458fef9e60205f8d2675bdd
Damian Kasyan <damian.kasyan@hotmail.com>
2018-03-26 09:55:17 +0200

    Poprawki log w jednym pliku

--- storestorm_centOS_7.sh
+++ storestorm_centOS_7.sh
@@ -11,46 +11,44 @@ sudo yum -y install mc
 sudo yum -y install net-tools 
 sudo yum -y install smartmontools 
 sudo yum -y install lftp 
+echo "Zainstalowałem wszystko co potrzebne" > logsto.txt
 lsblk 
-lsblk > str.txt
-sudo smartctl -t short /dev/sda > iaa.txt
-sudo smartctl -t short /dev/sdb > ibb.txt
-sudo smartctl -t shrot /dev/sdc > icc.txt
-sudo smartctl -t short /dev/sdd > idd.txt
+echo "Sprawdzam jakie dyski mam do sprawdzenia :)" >> logsto.txt
+lsblk >> logsto.txt
+sudo smartctl -t short /dev/sda >> logsto.txt
+sudo smartctl -t short /dev/sdb >> logsto.txt
+sudo smartctl -t shrot /dev/sdc >> logsto.txt
+sudo smartctl -t short /dev/sdd >> logsto.txt
+echo "Daj mi odpocząć 20 min" >> logsto.txt
 sleep 20m
-sudo smartctl -t long /dev/sda > iaal.txt
-sudo smartctl -t long /dev/sdb > ibbl.txt
-sudo smartctl -t long /dev/sdc > iccl.txt
-sudo smartctl -t long /dev/sdd > iddl.txt
-sleep 450m
+echo "Zaczynam sprawdzać dyski dokładniej" >> logsto.txt
+sudo smartctl -t long /dev/sda >> logsto.txt
+sudo smartctl -t long /dev/sdb >> logsto.txt
+sudo smartctl -t long /dev/sdc >> logsto.txt
+sudo smartctl -t long /dev/sdd >> logsto.txt
+echo "Ale się zmęczyłem, daj mi 450 min" >> logsto.txt
+sleep 450m 
+echo "Wypiszę ci co znalazłem do osobnych plików" >> logsto.txt
 sudo smartctl -a /dev/sda > testa.txt
 sudo smartctl -a /dev/sdb > testb.txt
 sudo smartctl -a /dev/sdc > testc.txt
 sudo smartctl -a /dev/sdd > testd.txt
 #Kasowanie danych na dyskach
-sudo shred -vf -n1 /dev/sda >dela.txt
-sudo shred -vf -n1 /dev/sdb >delb.txt
-sudo shred -vf -n1 /dev/sdc >delc.txt
-sudo shred -vf -n1 /dev/sdd >deld.txt
-echo "Skrypt został wykonany poprawnie"
+echo "Teraz trochę poczyszczę chwilę to zajmie" >> logsto.txt
+sudo shred -vf -n1 /dev/sda >> logsto.txt
+sudo shred -vf -n1 /dev/sdb >> logsto.txt
+sudo shred -vf -n1 /dev/sdc >> logsto.txt
+sudo shred -vf -n1 /dev/sdd >> logsto.txt
+echo "Skrypt został wykonany poprawnie, dozobaczenia następnym razem" >> logsto.txt
 
 
-lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put iaa.txt ; exit "
-lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put ibb.txt ; exit "
-lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put icc.txt ; exit "
-lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put idd.txt ; exit "
-lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put iaal.txt ; exit "
-lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put ibbl.txt ; exit "
-lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put iccl.txt ; exit "
-lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put iddl.txt ; exit "
+
+lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put logsto.txt ; exit "
 lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put testa.txt ; exit "
 lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put testb.txt ; exit "
 lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put testc.txt ; exit "
 lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put testd.txt ; exit "
-lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put dela.txt ; exit "
-lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put delb.txt ; exit "
-lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put delc.txt ; exit "
-lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put deld.txt ; exit "
+
 #lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put file ; exit "
 
 sudo shutdown -h 10
