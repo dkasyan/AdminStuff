d5ba423a26ad95096fe67705277a448f5e6d7819
Damian Kasyan <damian.kasyan@hotmail.com>
2018-04-18 13:46:02 +0200

    Update storestorm_debian_9.sh
    Poprawione na debiana

--- storestorm_debian_9.sh
+++ storestorm_debian_9.sh
@@ -22,24 +22,19 @@ lsblk
 chmod +777 logsto.txt
 echo "Sprawdzam jakie dyski mam do sprawdzenia :)" >> logsto.txt
 lsblk >> logsto.txt
-smartctl -t short /dev/sda >> logsto.txt
-smartctl -t short /dev/sdb >> logsto.txt
-smartctl -t shrot /dev/sdc >> logsto.txt
-smartctl -t short /dev/sdd >> logsto.txt
-echo "Daj mi odpocząć 20 min" >> logsto.txt
+smartctl -t short /dev/sda 
+smartctl -t short /dev/sdb 
+smartctl -t shrot /dev/sdc 
+smartctl -t short /dev/sdd 
+echo "Daj mi odpocząć 30 min" >> logsto.txt
 sleep 20m
 echo "Zaczynam sprawdzać dyski dokładniej" >> logsto.txt
-smartctl -t long /dev/sda >> logsto.txt
-smartctl -t long /dev/sdb >> logsto.txt
-smartctl -t long /dev/sdc >> logsto.txt
-smartctl -t long /dev/sdd >> logsto.txt
+smartctl -t long /dev/sda 
+smartctl -t long /dev/sdb 
+smartctl -t long /dev/sdc 
+smartctl -t long /dev/sdd 
 echo "Ale się zmęczyłem, daj mi 450 min" >> logsto.txt
 sleep 450m 
-echo "Wypiszę ci co znalazłem do osobnych plików" >> logsto.txt
-smartctl -a /dev/sda > testa.txt
-smartctl -a /dev/sdb > testb.txt
-smartctl -a /dev/sdc > testc.txt
-smartctl -a /dev/sdd > testd.txt
 #Kasowanie danych na dyskach
 echo "Teraz trochę poczyszczę chwilę to zajmie" >> logsto.txt
 shred -vf -n1 /dev/sda 
@@ -51,17 +46,23 @@ echo "Wyczyściłem dev/sdc" >> logsto.txt
 shred -vf -n1 /dev/sdd 
 echo "Wyczyściłem dev/sdd" >> logsto.txt
 echo "Skasowałem wszystkie dane" >> logsto.txt
+#Wypisanie danych do dysków 
+echo "Wypiszę ci co znalazłem do osobnych plików" >> logsto.txt
+smartctl -a /dev/sda > testaa.txt
+smartctl -a /dev/sdb > testbb.txt
+smartctl -a /dev/sdc > testcc.txt
+smartctl -a /dev/sdd > testdd.txt
 echo "Skrypt został wykonany poprawnie, dozobaczenia następnym razem" >> logsto.txt
 echo "Jeszcze tylko wyślę pliki i kończę"
 
 
 lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put logsto.txt ; exit "
-lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put testa.txt ; exit "
-lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put testb.txt ; exit "
-lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put testc.txt ; exit "
-lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put testd.txt ; exit "
+lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put testaa.txt ; exit "
+lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put testbb.txt ; exit "
+lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put testcc.txt ; exit "
+lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put testdd.txt ; exit "
 
 #lftp -u dftp,zaq1@WSX 192.168.150.96 -e "put file ; exit "
 echo "Elo"
 
-sudo shutdown -h 10
+
