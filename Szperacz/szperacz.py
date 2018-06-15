7190ef922f5a8d3cb62d56c26a29b16297a2a065
Damian Kasyan <damian.kasyan@gmail.com>
2018-06-15 10:45:08 +0200

    n

--- Szperacz/szperacz.py
+++ Szperacz/szperacz.py
@@ -3,14 +3,14 @@ import os
 
 poh = 50000
 
-print('Sprawdzam dyski pod kątem błędów i {} godzin'.format(poh))
+print('Sprawdzam dyski pod kątem błędów i liczby {} godzin'.format(poh))
 print('Podaj hasło administratora')
 #print("Sprawdzam dyski pod kontem {poh} godzin")
 
-#os.system('sudo smartctl -a /dev/sda > testt.txt ')
+#os.system('sudo smartctl -a /dev/sda > testda.txt ')
 
 
-with open('testt.txt','r') as f:                                 #Otwiera tekstgit
+with open('testaa.txt','r') as f:                                 #Otwiera tekstgit
         for line in f:
             p = line.find('Reallocated_Sector_Ct')
 
@@ -20,37 +20,52 @@ with open('testt.txt','r') as f:                                 #Otwiera tekstg
                 ile = slowa.count("")
                 ilei = int(ile)
                 slowai = int(slowa[35])
-                print(ile)
+                print("")
                 if slowai > 0:
-                    print("dysk do wyrzucenia")
+                    relsec = False
                 else:
-                    print("dysk smiga")
+                    relsec = True
+
+        for line in f:
+            p = line.find('Power_On_Hours')
+            if p > 0:
+                print(p)
 
 ############################# Sprawdzanie godzin ###############################
-with open('testt.txt','r') as f:                                 #Otwiera tekstgit
-        for line in f:
-            k = line.find('Power_On_Hours')
-            if k > 0:
-                del slowa
-                del slowai
-                del ile
-                del ilei
-                slowa = str(line)
-                slowa = line.split(" ")
-                print(slowa)
-                ile = slowa.count("")
-                print(slowa[44])
-                print()
+#with open('testda.txt','r') as f:                                 #Otwiera tekstgit
+#        for line in f:
+#            k = line.find('Power_On_Hours')
+#            if k > 0:
+#                del slowa
+#                del slowai
+#                del ile
+#                del ilei
+#                slowa = str(line)
+#                slowa = line.split(" ")
+#
+ #               ile = slowa.count("")
+                #print(slowa[43])
+
                 #ilei = int(ile)
-                #slowai = int(slowa[44])
+ #               slowai = int(slowa[43])
 
             #    print(ilei)
-            #    if slowai >= poh:
-                #    print("dysk do wyrzucenia")
-                #else:
-                #    print("dysk smiga")
-########################## Testy################################################
+#                if slowai >= poh:
+#                    powoh = False
+#                else:
+#                    powoh = True
+########################## Podsumowanie ################################################
+koniec = False
+if powoh == True:
+    if relsec == True:
+        koniec = True
+    else:
+        koniec = False
+else:
+    koniec = False
+if koniec == True:
+    os.system('sudo ')
+    print("Wszystko ok")
 
-        #        print(ile)
         #        print(slowa)
-        #        print(slowa[35])
+#        print(slowa[35])
