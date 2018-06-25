a50c83d00490fd9296f47154221f57613f8b5f70
Damian Kasyan <damian.kasyan@hotmail.com>
2018-06-25 13:40:59 +0200

    n

---
+++ bezpiecznik.py
@@ -0,0 +1,10 @@
+#Program robiący rsynkka 
+1. Program dodaje się do autostartu
+2. Program dodaje się do monitoringu
+3. Program robi 3 katalogi
+	a) KVM
+	b) Dedyki
+	c) ovz
+4. Program robi rsynki z kont 
+	a) Konto jest utworzone na serwerze backupowym
+	b) Odpalany jest tylko skrypt, z funkcją wykonywania. RSYNKUJE i SPRAWDZA parametry 
