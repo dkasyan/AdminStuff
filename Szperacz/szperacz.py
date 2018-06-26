66c9a1df74dab1e9bc2e528f8ed08382248624f2
Damian Kasyan <damian.kasyan@hotmail.com>
2018-06-26 11:00:26 +0200

    n

--- Szperacz/szperacz.py
+++ Szperacz/szperacz.py
@@ -18,7 +18,7 @@ with open('testaa.txt','r') as f:                                 #Otwiera tekst
             if k > 0:
                 words = str(line)
                 words = line.split(" ")
-                number = 20
+                number = 21
                 wordsi = words[number]
 
                 print(wordsi)
