ca39ef659de79db881ff8293b23a9a4cff3313d6
Damian Kasyan <damian.kasyan@hotmail.com>
2018-08-27 13:04:06 +0200

    n

--- audytor.py
+++ audytor.py
@@ -1,2 +1,11 @@
 import glob
-print(glob.glob("/backup/kvm/snetvps21-kvm/*.gz"))
+import re
+
+rootdir = "."
+prefix = "kvm"
+
+regex = re.compile(prefix + '([0-9]{3}_img_)\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])*.gz')
+
+nowekurwy = glob.glob("/backup/kvm/snetvps21-kvm/*.gz")
+
+print(nowekurwy)
