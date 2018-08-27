e074f4bc40992930bd8156791898fb1eb6ff8f9d
Damian Kasyan <damian.kasyan@hotmail.com>
2018-08-27 13:24:03 +0200

    n

--- audytor.py
+++ audytor.py
@@ -5,7 +5,11 @@ rootdir = "."
 prefix = "kvm"
 
 regex = re.compile(prefix + '([0-9]{3}_img_)\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])*.gz')
+szereg = []
 
 nowekurwy = glob.glob("/backup/kvm/snetvps21-kvm/*.gz")
 
-print(nowekurwy)
+for kurwa in nowekurwy:
+    print(regex.match(kurwa))
+
+
