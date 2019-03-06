c5d13bcf0fb2d2c4226669436dcd20ee078a621c
dkasyan <damian.kasyan@hotmail.com>
2019-03-06 18:01:44 +0100

    Poczatek skrupcioru do Nagios

---
+++ Disk_Checker/dc.py
@@ -0,0 +1,13 @@
+
+
+testa = open("sda.txt","r")
+exit0 = testa.readlines()
+testa.close()
+
+print (len(exit0))
+print (exit0[1])
+for x in exit0:
+
+##TEST b dopisac analogicznie
+#for x in exit0:
+#    print(x)
