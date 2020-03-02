4c7995e95963444a4491c0e77eb0681a88ad7ed3
Damian Kasyan <damian.kasyan@hotmail.com>
2020-03-02 20:02:52 +0000

    Add:dht11.py

--- dht11.py
+++ dht11.py
@@ -6,9 +6,10 @@ DHT11_pin = 23
 path = '/home/pi'
 humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT11_pin)
 if humidity is not None and temperature is not None:
-  print('Temperature={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-  file = open("dht11_data.txt", "a")
-  file.write("tekst \n")
+  dane = ('Temperature={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+  print(dane)
+  file = open("dht11_data.log", "a")
+  file.write( dane + "\n")
   file.close()
   
 else:
