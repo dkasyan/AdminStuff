85c753bd83eb23c6befb2fe6dad6ea9f133b8114
Damian Kasyan <damian.kasyan@hotmail.com>
2020-03-03 20:50:03 +0000

    Add: TimeStamp to log

--- dht11.py
+++ dht11.py
@@ -1,15 +1,16 @@
 import Adafruit_DHT
 import os
+from datetime import datetime
 
+time = datetime.now()
 sensor = Adafruit_DHT.DHT11
 DHT11_pin = 23
-path = '/home/pi'
+path = '/home/pi/AdminStuff'
 humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT11_pin)
 if humidity is not None and temperature is not None:
   dane = ('Temperature={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-  print(dane)
   file = open("dht11_data.log", "a")
-  file.write( dane + "\n")
+  file.write(str(time) + " " + dane + "\n")
   file.close()
   
 else:
